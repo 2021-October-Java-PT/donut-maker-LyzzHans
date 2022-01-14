1168f00e3544b9a0a4c451e65c9ef324b5881ae1
LyzzHans <lyzz.hans@gmail.com>
2022-01-14 16:50:39 -0500

    design

--- donut-clicker-app.js
+++ donut-clicker-app.js
@@ -1,13 +1,15 @@
 import DonutMaker from './donut-clicker.js';
 
-const donutBtn = document.getElementById('donutBtn');
+// const donutBtn = document.getElementById('donutBtn');
 
-
+const counterBoard = document.getElementById("counter-board");
       
+let counterValue = 0;
+
+const donutBtn = () =>{
+  counterBoard.innerHTML = ++counterValue;
+}
 
 
-
-
-
-
+donutBtn.addEventListener('click', increaseCounter);
   
