16988acc91f573c425f94256db93eaf987a01072
LyzzHans <lyzz.hans@gmail.com>
2022-01-16 11:00:53 -0500

    clicker works

--- donut-clicker-app.js
+++ donut-clicker-app.js
@@ -1,15 +1,33 @@
-import DonutMaker from './donut-clicker.js';
+import DonutClicker from './donut-clicker';
 
-// const donutBtn = document.getElementById('donutBtn');
+renderPage();
 
-const counterBoard = document.getElementById("counter-board");
-      
-let counterValue = 0;
 
-const donutBtn = () =>{
-  counterBoard.innerHTML = ++counterValue;
+function renderPage() {
+  bakeDonut();
+}
+function bakeDonut(){
+
+
+const counterBoard = document.querySelector('#donut-counter');
+const donutBtn = document.querySelector('#bakeDonut');
+const donutCntDisp = document.querySelector('#donutCntr');
+const donutClicker = new DonutClicker();
+console.log('check in 1');
+donutBtn.addEventListener('click', () => {
+console.log('check in 2');
+  donutClicker.bakeDonut();
+  donutCntDisp.innerText = donutClicker.donutCount;//important method and listener for autoclick
+console.log(donutClicker.donutCount);
+  
+
+
+// const resetCounter =() =>{
+
+//   counterBoard.innerHTML = 0;
+//   donutCount = 0;
+// }
+// resetButton.addEventListener('click', resetCounter);
+});
 }
 
-
-donutBtn.addEventListener('click', increaseCounter);
-  
