76c963469a7301e18dc0a2de61ffdd165420e545
LyzzHans <lyzz.hans@gmail.com>
2022-01-18 19:18:49 -0500

    multiplier function

--- donut-clicker-app.js
+++ donut-clicker-app.js
@@ -9,25 +9,49 @@ function renderPage() {
 function bakeDonut(){
 
 
-const counterBoard = document.querySelector('#donut-counter');
+
 const donutBtn = document.querySelector('#bakeDonut');
+const buyAutoClicker =document.querySelector("#buyAutoClicker");
 const donutCntDisp = document.querySelector('#donutCntr');
-const donutClicker = new DonutClicker();
+const multiBtn = document.querySelector('#multiplierBtn');
+const resetBtn = document.querySelector('#reset-btn');
+const donutClicker = new DonutClicker(0,0,100,1,0,10);
+
+
+
 console.log('check in 1');
 donutBtn.addEventListener('click', () => {
+
 console.log('check in 2');
   donutClicker.bakeDonut();
-  donutCntDisp.innerText = donutClicker.donutCount;//important method and listener for autoclick
+  donutCntDisp.innerText = donutClicker.donutCount;
 console.log(donutClicker.donutCount);
-  
-
-
-// const resetCounter =() =>{
-
-//   counterBoard.innerHTML = 0;
-//   donutCount = 0;
-// }
-// resetButton.addEventListener('click', resetCounter);
 });
-}
 
+buyAutoClicker.addEventListener("click", () => {
+  
+donutClicker.buyAutoClicker();
+donutCntDisp.innerText = donutClicker.donutCount;
+autoCntDisp.innerText = donutClicker.autoClickCount;
+    
+    });
+ 
+  multiBtn.addEventListener('click', () =>{
+      
+    donutClicker.buyMultiplier();
+  
+  donutCntDisplay.innerText = donutClicker.donutCount;
+  autoCntDisplay.innerText = donutClicker.autoClickCount;
+  multiCntDisplay.innerText = donutClicker.multiplierCount;
+});
+ autoClickBtn.addEventListener('click', () =>{
+  for (let i = 0; i < donut.autoClicks; i++) {
+    donutClicker.bakeDonut();
+  }
+  donutClicker.donutCount();
+});
+  resetBtn.addEventListener("click", () => {
+    location.reload();
+
+  });
+}
--- donut-clicker.js
+++ donut-clicker.js
@@ -5,21 +5,32 @@ class DonutClicker{
 
     constructor() {
         this.donutCount = 0;
-    }
-
-    getDonutCount() {
-        return this.donutCount;
+        this.autoClicks = 0;
+        this.autoClicksCost = 100;
+        this.multiplier = 1;
+        this.multiplierCount = 0;
+        this.multiplierCost = 10;
+       
+       
     }
 
     bakeDonut() {
-        this.donutCount += 1;
+        this.donutCount += this.multiplier;
     }
 
-
-
-
-
-
-
+buyAutoClicker() {
+  this.donutCount = Math.round(this.donutCount - this.autoClicksCost);
+  this.autoClicks += 1;
 }
+buyMultiplier() {
+  this.donutCount = Math.round(this.count - this.multiplierCost);
+  this.multiplier = this.multiplier * 1.2;
+  this.multiplierCount += 1;
+}
+}
+
+reset() 
+  location.reload;
+
+
 export default DonutClicker;
